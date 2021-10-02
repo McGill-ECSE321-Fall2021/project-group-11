4058be44e7ce47664f6736b442c299189478ee2c
Paul Teng <plankp@outlook.com>
2021-10-02 08:25:32 -0400

    Generate project structure using spring
    As for the exact command executed:
    
    ```
    spring init \
     --build=gradle \
     --java-version=1.8 \
     --package=ca.mcgill.ecse321.townlibrary \
     --name=TownLibrary \
     --dependencies=web,data-jpa,postgresql \
     TownLibrary-Backend
    ```

---
+++ TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/TownLibraryApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.townlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownLibraryApplication.class, args);
+	}
+
+}
