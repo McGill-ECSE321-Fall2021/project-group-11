40c3f0a040afa2c59873bfd76fbf4b84dc3071bb
Paul Teng <plankp@outlook.com>
2021-10-02 09:12:28 -0400

    Add dummy endpoint

--- TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/TownLibraryApplication.java
+++ TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/TownLibraryApplication.java
@@ -2,7 +2,10 @@ package ca.mcgill.ecse321.townlibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class TownLibraryApplication {
 
@@ -10,4 +13,8 @@ public class TownLibraryApplication {
 		SpringApplication.run(TownLibraryApplication.class, args);
 	}
 
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello! App is running!";
+    }
 }
