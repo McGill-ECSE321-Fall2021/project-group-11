a3c093f8845e965123ba989e248a917d0fa32dc5
Paul Teng <plankp@outlook.com>
2021-10-12 17:51:55 -0400

    Cleanup ugly reflection black-magic
    (it will get removed anyway... eventually)

--- TownLibrary-Backend/src/test/java/ca/mcgill/ecse321/townlibrary/repository/ArchiveRepositoryTest.java
+++ TownLibrary-Backend/src/test/java/ca/mcgill/ecse321/townlibrary/repository/ArchiveRepositoryTest.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse321.townlibrary.repository;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,25 @@ import java.util.*;
 
 @SpringBootTest
 public class ArchiveRepositoryTest {
+
+    // XXX: remove this when accessors are implemented for Item class
+    private static java.lang.reflect.Field field_id;
+    private static java.lang.reflect.Field field_status;
+    private static java.lang.reflect.Field field_name;
+    private static java.lang.reflect.Field field_library;
+
+    @BeforeAll
+    public static void temporaryReflectionMagic() throws Exception {
+        field_id = Item.class.getDeclaredField("id");
+        field_status = Item.class.getDeclaredField("status");
+        field_name = Item.class.getDeclaredField("name");
+        field_library = Item.class.getDeclaredField("library");
+
+        field_id.setAccessible(true);
+        field_status.setAccessible(true);
+        field_name.setAccessible(true);
+        field_library.setAccessible(true);
+    }
 
     // XXX: remove this when repos for other classes are created!
     @Autowired
@@ -29,51 +49,25 @@ public class ArchiveRepositoryTest {
 
     @Test
     @Transactional // XXX: same as entity manager
-    public void testPersistArchive() {
-        // XXX: when accessors are implemented for Item class
-        java.lang.reflect.Field field_id = null;
-        java.lang.reflect.Field field_status = null;
-        java.lang.reflect.Field field_name = null;
-        java.lang.reflect.Field field_library = null;
-        try {
-            field_id = Item.class.getDeclaredField("id");
-            field_status = Item.class.getDeclaredField("status");
-            field_name = Item.class.getDeclaredField("name");
-            field_library = Item.class.getDeclaredField("library");
-            field_id.setAccessible(true);
-            field_status.setAccessible(true);
-            field_name.setAccessible(true);
-            field_library.setAccessible(true);
-        } catch (Exception ex) {
-            Assertions.fail("Reflection issues (should not happen)");
-        }
-
+    public void testPersistArchive() throws Exception {
         final Library lib = new Library();
         this.entityManager.persist(lib);
         this.entityManager.flush();
 
         // Test writes
         final Archive stArchive = new Archive();
-        try {
-            field_id.setInt(stArchive, 10);
-            field_status.set(stArchive, Status.RESERVED);
-            field_name.set(stArchive, "FooBar");
-            field_library.set(stArchive, lib);
-        } catch (Exception ex) {
-            Assertions.fail("Reflection issues (should not happen)");
-        }
+        field_id.setInt(stArchive, 10);
+        field_status.set(stArchive, Status.RESERVED);
+        field_name.set(stArchive, "FooBar");
+        field_library.set(stArchive, lib);
         this.archiveRepository.save(stArchive);
 
         // this get must succeed!
         final Archive ldArchive = this.archiveRepository.findById(10).get();
-        try {
-            Assertions.assertEquals(10, field_id.getInt(ldArchive));
-            Assertions.assertEquals(Status.RESERVED, field_status.get(stArchive));
-            Assertions.assertEquals("FooBar", field_name.get(stArchive));
-            Assertions.assertEquals(lib, field_library.get(stArchive));
-        } catch (Exception ex) {
-            Assertions.fail("Reflection issues (should not happen)");
-        }
+        Assertions.assertEquals(10, field_id.getInt(ldArchive));
+        Assertions.assertEquals(Status.RESERVED, field_status.get(stArchive));
+        Assertions.assertEquals("FooBar", field_name.get(stArchive));
+        Assertions.assertEquals(lib, field_library.get(stArchive));
 
         // Test deletes
         this.archiveRepository.delete(ldArchive);
