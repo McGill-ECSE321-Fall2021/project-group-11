27ac2a821bb271f27138911917249ec85593ef5a
Paul Teng <plankp@outlook.com>
2021-10-12 11:04:12 -0400

    Generate Project Template (#22)
    * Generate project structure using spring
    
    As for the exact command executed:
    
    ```
    spring init \
     --build=gradle \
     --java-version=1.8 \
     --package=ca.mcgill.ecse321.townlibrary \
     --name=TownLibrary \
     --dependencies=web,data-jpa,postgresql \
     TownLibrary-Backend
    ```
    
    * Create Github Workflow for backend
    
    This is mostly the same file as the one shown in the tutorial with some differences:
    *  Builds on branch 'main' instead of 'master'
    *  Runs on ubuntu instead of windows
    *  Currently does not deploy to heroku
    
    * Integrate with heroku
    
    * Add dummy endpoint
    
    * Add profile for local
    
    You can switch profiles by exporting the SPRING_PROFILES_ACTIVE key.
    
    (intelliJ ultimate has a setting to do this also)
    
    * Add automated deployment logic to actions
    
    This is based on the stuff shown during Ding Ma's tutorials

---
+++ TownLibrary-Backend/src/test/java/ca/mcgill/ecse321/townlibrary/TownLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.townlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
