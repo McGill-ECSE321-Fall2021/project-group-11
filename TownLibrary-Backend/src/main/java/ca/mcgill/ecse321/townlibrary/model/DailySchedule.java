0dfde06cb868521c8ad1ec69d003326f94c8af33
James Chau <jchau@localhost.localdomain>
2021-10-15 00:27:34 -0400

    Added getters/setters to DailySchedule and OnlineMember

--- TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/model/DailySchedule.java
+++ TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/model/DailySchedule.java
@@ -20,4 +20,53 @@ public class DailySchedule {
 
     @ManyToOne
     private Library library;
+
+    public int getId() {
+        return id;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+    
+    public Librarian getLibrarian() {
+        return librarian;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setLibrarian(Librarian librarian) {
+        this.librarian = librarian;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
 }
--- TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/model/OnlineMember.java
+++ TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/model/OnlineMember.java
@@ -11,4 +11,29 @@ public class OnlineMember extends UserRole {
     private String username;
 
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
