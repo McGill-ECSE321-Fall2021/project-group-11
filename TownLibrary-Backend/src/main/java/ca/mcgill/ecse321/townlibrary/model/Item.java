0337b5fd48348668a52c244e2131ccada2d1d726
Paul Teng <plankp@outlook.com>
2021-10-16 11:39:45 -0400

    Add library back as ManyToOne

--- TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/model/Item.java
+++ TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/model/Item.java
@@ -9,15 +9,16 @@ public abstract class Item {
     private int id;
     private Status status;
     private String name;
-    
+
     private Transaction transaction;
-    
-    
+
+    private Library library;
+
     @Id
     public int getId() {
     	return this.id;
     }
-    
+
     public void setId(int value) {
     	this.id = value;
     }
@@ -25,27 +26,35 @@ public abstract class Item {
     public Status getStatus() {
     	return this.status;
     }
-    
+
     public void setStatus(Status value) {
     	this.status = value;
     }
-    
+
     public String getName() {
     	return this.name;
     }
-    
+
     public void setName(String value) {
     	this.name = value;
     }
 
-    
     @OneToOne
     public Transaction getTransaction() {
     	return this.transaction;
     }
-    
+
     public void setTransaction(Transaction transaction) {
     	this.transaction = transaction;
+    }
+
+    @ManyToOne
+    public Library getLibrary() {
+    	return this.library;
+    }
+
+    public void setLibrary(Library library) {
+    	this.library = library;
     }
 
 }
