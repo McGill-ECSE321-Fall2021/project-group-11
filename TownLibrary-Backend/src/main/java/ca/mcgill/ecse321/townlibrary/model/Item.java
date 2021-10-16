46fd98a4f999c33c0cf75aeb0b468711c453f07b
brimless <minh-tuan.chau@mail.mcgill.ca>
2021-10-16 03:59:28 -0400

    Pesistence Layer and Test cases for DailySchedule, MusicAlbum and OnlineMember

--- TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/model/Item.java
+++ TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/model/Item.java
@@ -6,13 +6,46 @@ import java.util.*;
 @Entity
 public abstract class Item {
 
-    @Id
     private int id;
-
     private Status status;
-
     private String name;
+    
+    private Transaction transaction;
+    
+    
+    @Id
+    public int getId() {
+    	return this.id;
+    }
+    
+    public void setId(int value) {
+    	this.id = value;
+    }
 
-    @ManyToOne
-    private Library library;
+    public Status getStatus() {
+    	return this.status;
+    }
+    
+    public void setStatus(Status value) {
+    	this.status = value;
+    }
+    
+    public String getName() {
+    	return this.name;
+    }
+    
+    public void setName(String value) {
+    	this.name = value;
+    }
+
+    
+    @OneToOne
+    public Transaction getTransaction() {
+    	return this.transaction;
+    }
+    
+    public void setTransaction(Transaction transaction) {
+    	this.transaction = transaction;
+    }
+
 }
