552e65ce5af4d400d8e25d5c86730907cbd9ba8e
Quang Loc Tran <quang.l.tran@mail.mcgill.ca>
2021-10-16 13:04:39 -0400

    Typos

--- TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/repository/OfflineMemberRepository.java
+++ TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/repository/OfflineMemberRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface OfflineMemberRepository extends CrudRepository<Archive, Integer> {
+public interface OfflineMemberRepository extends CrudRepository<OfflineMember, Integer> {
     
     List<OfflineMember> findByAddress(String name);
 }
