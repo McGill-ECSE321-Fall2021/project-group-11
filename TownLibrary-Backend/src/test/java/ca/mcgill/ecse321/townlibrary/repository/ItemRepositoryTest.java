0257f7b30c145b788860d53ea351c9bfda389851
Shu Tian <shuop@hotmail.com>
2021-10-16 12:34:30 -0400

    Item tests

--- TownLibrary-Backend/src/test/java/ca/mcgill/ecse321/townlibrary/repository/ItemRepositoryTest.java
+++ TownLibrary-Backend/src/test/java/ca/mcgill/ecse321/townlibrary/repository/ItemRepositoryTest.java
@@ -1,5 +1,8 @@
 package ca.mcgill.ecse321.townlibrary.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,25 +23,51 @@ public class ItemRepositoryTest {
 	
 	@Test
 	public void testPersistAndLoadItem() {
-		/*
+		Integer id1 = 3301;
+		String name1 = "Gazette";
+		Status status1 = Status.AVAILABLE;
+		Transaction transaction1 = new Transaction();
 		Newspaper gazette = new Newspaper();
-		gazette.setId(3301);
-		gazette.setName("Gazette");
-		gazette.setStatus(Status.AVAILABLE);
+		gazette.setId(id1);
+		gazette.setName(name1);
+		gazette.setStatus(status1);
+		gazette.setTransaction(transaction1);
 		itemRepository.save(gazette);
 		
+		Integer id2 = 2501;
+		String name2 = "Dune";
+		Status status2 = Status.RESERVED;
+		Transaction transaction2 = new Transaction();
 		Book dune = new Book();
-		dune.setId(2501);
-		dune.setName("Dune");
-		dune.setStatus(Status.CHECKED_OUT);
+		dune.setId(id2);
+		dune.setName(name2);
+		dune.setStatus(status2);
+		dune.setTransaction(transaction2);
 		itemRepository.save(dune);
 		
+		Integer id3 = 1991;
+		String name3 = "Flowers for Algernon";
+		Status status3 = Status.RESERVED;
+		Transaction transaction3 = new Transaction();
 		Book algernon = new Book();
-		algernon.setId(1991);
-		algernon.setName("Flowers for Algernon");
-		algernon.setStatus(Status.RESERVED);
+		algernon.setId(id3);
+		algernon.setName(name3);
+		algernon.setStatus(status3);
+		algernon.setTransaction(transaction3);
 		itemRepository.save(algernon);
-		*/
+		
+		Item item;
+		item = itemRepository.findItemById(id1);
+		assertNotNull(item);
+		assertEquals(id1, item.getId());
+		
+		item = itemRepository.findItemById(id2);
+		assertNotNull(item);
+		assertEquals(id2, item.getId());
+		
+		item = itemRepository.findItemByTransaction(transaction3);
+		assertNotNull(item);
+		assertEquals(transaction3, item.getTransaction());
 		
 	}
 
