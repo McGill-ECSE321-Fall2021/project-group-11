208c6b4e4616a1b4c1971bd32d8ef5e725722beb
Quang Loc Tran <quang.l.tran@mail.mcgill.ca>
2021-10-17 17:10:47 -0400

    Added more tests to OfflineMember

--- TownLibrary-Backend/src/test/java/ca/mcgill/ecse321/townlibrary/repository/OfflineMemberRepositoryTest.java
+++ TownLibrary-Backend/src/test/java/ca/mcgill/ecse321/townlibrary/repository/OfflineMemberRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.*;
+
 import ca.mcgill.ecse321.townlibrary.model.*;
 
 @SpringBootTest
@@ -54,6 +56,50 @@ public class OfflineMemberRepositoryTest {
         this.offlineMemberRepository.delete(om);
         Assertions.assertTrue(this.offlineMemberRepository.
                                 findByAddress("845 Jacoma").isEmpty());
+    }
+    public void testPersistMultipleOfflineMember(){
+        // Create Library Instance
+        String address = "845 Rue Sherbrooke";
+        Library lib = new Library();
+        int libId = 1002;
+        lib.setId(libId);
+        lib.setAddress(address);
+        this.libraryRepository.save(lib);
+
+        // Create offline member to test
+        OfflineMember om = new OfflineMember();
+        om.setAddress("845 Jacoma");
+        int omId = 4;
+        om.setId(omId);
+        om.setLibrary(lib);
+        om.setName("Wu");
+        this.offlineMemberRepository.save(om);
+
+        // Create another offline member to test
+        OfflineMember om1 = new OfflineMember();
+        om1.setAddress("845 Jacoma");
+        int om1Id = 5;
+        om1.setId(om1Id);
+        om1.setLibrary(lib);
+        om1.setName("Wutang");
+        this.offlineMemberRepository.save(om1);
+
+        // Create another offline member to test
+        OfflineMember om2 = new OfflineMember();
+        om2.setAddress("846 Jacoma");
+        int om2Id = 6;
+        om2.setId(om2Id);
+        om2.setLibrary(lib);
+        om2.setName("Wutang");
+        this.offlineMemberRepository.save(om2);
+
+        List<OfflineMember> oms = this.offlineMemberRepository.findByAddress("845 Jacoma");
+        assertFalse(oms.isEmpty());
+        assertEquals(2, oms.size());
+        assertEquals(
+            new HashSet<>(Arrays.asList(4,5)),
+            new HashSet<>(Arrays.asList(oms.get(0).getId(), oms.get(1).getId()))
+        );
     }
     
 }
