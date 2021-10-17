e2111f948274fba6bda40316918c722eca425052
Quang Loc Tran <quang.l.tran@mail.mcgill.ca>
2021-10-17 12:26:23 -0400

    Update HeadLibrarianRepository.java

--- TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/repository/HeadLibrarianRepository.java
+++ TownLibrary-Backend/src/main/java/ca/mcgill/ecse321/townlibrary/repository/HeadLibrarianRepository.java
@@ -4,8 +4,6 @@ import ca.mcgill.ecse321.townlibrary.model.*;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface HeadLibrarianRepository extends CrudRepository<HeadLibrarian, Integer> {
 /**
