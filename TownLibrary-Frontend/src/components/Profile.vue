deec7acc3b0e37956a49f0d936411e96ece1828d
Paul Teng <plankp@outlook.com>
2021-11-19 20:55:00 -0500

    Feature/librarian mgmt (#102)
    * We be creating librarians
    
    * We be deleting as well
    
    Horray! Async await is beautiful!
    
    * Rewrote some profile stuff with async
    
    (Only migrating some, in case people are freaked out by it)
    
    * Add error message handling

--- TownLibrary-Frontend/src/components/Profile.vue
+++ TownLibrary-Frontend/src/components/Profile.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="profile">
+    <h2>Welcome back {{ userInfo.name }}!</h2>
+    <button v-on:click="doLogout">Logout</button>
+
     <p>Just to show that stuff does work:</p>
     <ul>
       <li>User type: {{ loginStatus.userType }}</li>
@@ -7,17 +10,52 @@
       <li>Password: {{ loginStatus.password }}</li>
     </ul>
 
-    <button v-on:click="doLogout">Logout</button>
+    <div v-if="'head-librarian' === loginStatus.userType">
+      List of Librarians:
+      <button v-on:click="reloadLibrarians()">Refresh</button>
+      <table>
+        <tr>
+          <th><!-- Empty cell just for aligning the table --></th>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Address</th>
+        </tr>
+        <tr v-for="librarian in (librarians || [])">
+          <td>
+            <button v-bind:disabled="librarian.id === userInfo.id"
+                    v-on:click="deleteLibrarian(librarian.id)">Delete</button>
+          </td>
+          <td>{{ librarian.id }}</td>
+          <td>{{ librarian.name }}</td>
+          <td>{{ librarian.address }}</td>
+        </tr>
+      </table>
+      <p style="color: red" v-if="librarians == null">Failed to load from server, try again later by clicking on refresh</p>
+
+      <button v-on:click="$router.push('hire')">Add a new librarian</button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+var frontendUrl = 'http://' + process.env.FRONTEND_HOST + ':' + process.env.FRONTEND_PORT
+var backendUrl = 'http://' + process.env.API_HOST + ':' + process.env.API_PORT
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
 export default {
   name: 'profile',
 
   data () {
     return {
-      loginStatus: {}
+      loginStatus: {},
+      userInfo: {},
+      librarians: []
     }
   },
 
@@ -25,12 +63,53 @@ export default {
     // Copy the login status out so that it does not cause issues when logout
     // is requested.
     Object.assign(this.loginStatus, this.$store.state.loginStatus)
+
+    this.reloadUserInfo()
+    this.reloadLibrarians()
   },
 
   methods: {
     doLogout () {
       this.$store.commit('logout')
       this.$router.push('login')
+    },
+
+    async reloadUserInfo () {
+      try {
+        let response = await AXIOS.post('/auth/' + this.loginStatus.userType + 's/' + this.loginStatus.username, null, {
+          params: {
+            password: this.loginStatus.password
+          }
+        })
+        this.userInfo = response.data
+      } catch (error) {
+        // this is awkward because we couldn't get the user's information...
+        // assume the worst (maybe the password has changed or something) and
+        // logout the user.
+        this.doLogout()
+      }
+    },
+
+    async reloadLibrarians () {
+      try {
+        let response = await AXIOS.get('/librarians')
+        this.librarians = response.data
+      } catch (error) {
+        this.librarians = null
+      }
+    },
+
+    async deleteLibrarian (id) {
+      // trigger the delete event
+      await AXIOS.delete('/librarians/' + id, {
+        params: {
+          initId: this.loginStatus.username,
+          initPass: this.loginStatus.password
+        }
+      })
+
+      // and try reloading the list regardless of success or failure
+      this.reloadLibrarians()
     }
   }
 }
