58a3d42438c9ff787f16133e38c269abebe74475
Paul Teng <plankp@outlook.com>
2021-11-29 11:41:52 -0500

    Add several empty fragments based on website toolbar

---
+++ TownLibrary-Android/app/src/main/java/ca/mcgill/ecse321/townlibrary/ItemsFragment.java
@@ -0,0 +1,48 @@
+package ca.mcgill.ecse321.townlibrary;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Set;
+
+import ca.mcgill.ecse321.townlibrary.databinding.FragmentItemsBinding;
+import cz.msebera.android.httpclient.Header;
+
+public class ItemsFragment extends Fragment {
+
+    private FragmentItemsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentItemsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // TODO:
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ TownLibrary-Android/app/src/main/java/ca/mcgill/ecse321/townlibrary/ScheduleFragment.java
@@ -0,0 +1,48 @@
+package ca.mcgill.ecse321.townlibrary;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Set;
+
+import ca.mcgill.ecse321.townlibrary.databinding.FragmentScheduleBinding;
+import cz.msebera.android.httpclient.Header;
+
+public class ScheduleFragment extends Fragment {
+
+    private FragmentScheduleBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentScheduleBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // TODO:
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
