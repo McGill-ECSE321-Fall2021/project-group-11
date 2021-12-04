d0252c12cd3ffd106ae07d5d7fc65416d6771a53
Quang Loc Tran <quang.l.tran@mail.mcgill.ca>
2021-12-03 20:32:12 -0500

    Nav complete for Member profile

--- TownLibrary-Android/app/src/main/java/ca/mcgill/ecse321/townlibrary/PersonalFragment.java
+++ TownLibrary-Android/app/src/main/java/ca/mcgill/ecse321/townlibrary/PersonalFragment.java
@@ -1,2 +1,39 @@
-package ca.mcgill.ecse321.townlibrary;public class PersonalFragment {
+package ca.mcgill.ecse321.townlibrary;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ca.mcgill.ecse321.townlibrary.databinding.FragmentPersonalBinding;
+
+public class PersonalFragment extends Fragment {
+
+    private FragmentPersonalBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentPersonalBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // TODO:
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
 }
